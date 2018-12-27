17a6145518614049a29b713d6e66b0a826f398e3
lizhanzhan <425225007@qq.com>
2018-12-27 10:58:55 +0800

    链表

--- main.go
+++ main.go
@@ -2,15 +2,35 @@ package main
 
 import (
 	"fmt"
-	letcode "suanfa/letcode"
+	"suanfa/letcode"
 )
 
 func main() {
-	letcode.TwoSumList()
-	list := letcode.List{}
-	list.Add(122)
-	list.Add(2222)
+	list1 := letcode.ListNode{}
+	list1.ListAdd(1)
 
-	fmt.Println()
+	//travselLinkList(&list1)
 
+	list2 := letcode.ListNode{}
+	list2.ListAdd(9)
+	list2.ListAdd(9)
+
+	//travselLinkList(&list2)
+
+	res := letcode.AddTwoNumbers(&list1, &list2)
+	_ = res
+
+	//fmt.Println(10 % 10)
+	//fmt.Println(6 / 10)
+
+	travselLinkList(res)
+}
+
+func travselLinkList(list *letcode.ListNode) {
+	//遍历
+	head := list
+	for head != nil {
+		fmt.Println(head.Val)
+		head = head.Next
+	}
 }
