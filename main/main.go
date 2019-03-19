8e5a6ae3bc56028fc41d6436c2669be937d9d3a1
lizhanzhan <425225007@qq.com>
2019-03-19 19:46:34 +0800

    链表的合并

--- main/main.go
+++ main/main.go
@@ -1,16 +1,47 @@
 package main
 
 import (
-	"algorithm/letcode"
 	"fmt"
 )
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+type List struct {
+	headNode *ListNode //头节点
+}
 
 func main() {
 
 	//res := letcode.Divide(434, 423)
 	//fmt.Println(res)
-	nums1 := []int{1, 1}
-	res := letcode.SearchRange(nums1, 1)
-	fmt.Println(res)
+	list := ListNode{}
+	list.Add(1)
+	list.Add(2)
+	list.Add(3)
+	list.Add(4)
+	fmt.Println(list)
+	//res := letcode.MergeTwoLists(list., list1)
+	//fmt.Println(res)
+}
+
+//从头部添加元素
+func (this *ListNode) Add(data int) {
+	node := &ListNode{Val: data, Next: this.Next}
+	this.Val = data
+
+	this.Next = node
+}
+
+//插入节点
+func Insert(value int, list **ListNode) {
+	stu := ListNode{
+		Val: 2,
+	}
+
+	stu.Next = *list
+
+	*list = &stu
 
 }
