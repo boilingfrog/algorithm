41db07155a0d4a082adf50c21b94d2e250ee9a35
lizhan <425225007@qq.com>
2019-03-20 23:28:34 +0800

    单链表

--- letcode/2_two_sum_list.go
+++ letcode/2_two_sum_list.go
@@ -24,10 +24,10 @@ import "fmt"
  * }
  */
 
-type ListNode struct {
-	Val  int
-	Next *ListNode
-}
+//type ListNode struct {
+//	Val  int
+//	Next *ListNode
+//}
 
 func AddTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 	resNode := ListNode{}
