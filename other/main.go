fdb49edd1dc3cb331d883cad97d1a5b1ca0ad038
lz-2019 <rick.lizhan@gmail.com>
2020-09-13 15:49:55 +0800

    list

--- other/main.go
+++ other/main.go
@@ -5,5 +5,16 @@ import "fmt"
 func main() {
 	fmt.Println(212)
 
-	lru()
+	// list
+
+	headNode := &ListNode{}
+
+	listData := headNode
+	Insert(1, listData, headNode)
+	Insert(2, listData, headNode)
+	Insert(4, listData, headNode)
+
+	PrintList(listData)
+
+	fmt.Println(DoublePointer(listData, 2))
 }
