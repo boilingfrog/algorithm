1f80a97c6b8b5ed43cee43ee7ec83ff76cb58831
liz <rick.lizhan@gmail.com>
2021-07-01 13:07:17 +0800

    search_insert

--- main.go
+++ main.go
@@ -7,7 +7,11 @@ import (
 
 func main() {
 
-	fmt.Println(letcode.MaxSubArray([]int{
-		-2, -1,
-	}))
+	fmt.Println(letcode.SearchMatrix([][]int{
+		{1, 4, 7, 11, 15},
+		{2, 5, 8, 12, 19},
+		{3, 6, 9, 16, 22},
+		{10, 13, 14, 17, 24},
+		{18, 21, 23, 26, 30},
+	}, 5))
 }
