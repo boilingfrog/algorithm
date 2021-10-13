86f3deb72e2b5e32276b96578737631be4a7723f
liz <rick.lizhan@gmail.com>
2021-10-13 13:20:56 +0800

    二分查找，查找最后一个等于给定值的元素

--- sort/bsearch.go
+++ sort/bsearch.go
@@ -78,3 +78,33 @@ func bsearchFirstEqual(arr []int, target int) int {
 
 	return findFirstEqual(arr, target, 0, len(arr)-1)
 }
+
+// ---------------------------------------------------------------------------------------------------------------------
+// 查找最后一个值等于给定值的元素
+func findLastEqual(arr []int, target, left, right int) int {
+	if left > right {
+		return -1
+	}
+	mid := left + (right-left)/2
+	if arr[mid] == target {
+		if mid < len(arr) && arr[mid+1] == target {
+			left = mid + 1
+		} else {
+			return mid
+		}
+	} else if arr[mid] < target {
+		left = mid + 1
+	} else {
+		right = mid - 1
+	}
+
+	return findLastEqual(arr, target, left, right)
+}
+
+func bsearchLastEqual(arr []int, target int) int {
+	if len(arr) == 0 {
+		return -1
+	}
+
+	return findLastEqual(arr, target, 0, len(arr)-1)
+}
