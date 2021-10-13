7c6cbb7de5f8ad51f4041f9ccd924ab2a8bb413d
liz <rick.lizhan@gmail.com>
2021-10-13 13:11:52 +0800

    二分查找，查找第一个等于给定值的元素

--- sort/bsearch.go
+++ sort/bsearch.go
@@ -48,3 +48,33 @@ func bsearch(arr []int, target int) int {
 
 	return bsearchDigui(arr, target, 0, len(arr)-1)
 }
+
+// ---------------------------------------------------------------------------------------------------------------------
+// 查找第一个值等于给定值的元素
+func findFirstEqual(arr []int, target, left, right int) int {
+	if left > right {
+		return -1
+	}
+	mid := left + (right-left)/2
+	if arr[mid] == target {
+		if mid >= 0 && arr[mid-1] == target {
+			right = mid - 1
+		} else {
+			return mid
+		}
+	} else if arr[mid] < target {
+		left = mid + 1
+	} else {
+		right = mid - 1
+	}
+
+	return findFirstEqual(arr, target, left, right)
+}
+
+func bsearchFirstEqual(arr []int, target int) int {
+	if len(arr) == 0 {
+		return -1
+	}
+
+	return findFirstEqual(arr, target, 0, len(arr)-1)
+}
