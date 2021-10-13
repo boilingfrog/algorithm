56cc7f109a8b3f5b405d7e8a9fbec0593ecc9be9
liz <rick.lizhan@gmail.com>
2021-10-13 09:20:45 +0800

    二分查找

---
+++ sort/bsearch.go
@@ -0,0 +1,23 @@
+package main
+
+func bsearch(arr []int, target int) int {
+	if len(arr) == 0 {
+		return -1
+	}
+
+	left := 0
+	right := len(arr) - 1
+
+	for left <= right {
+		mid := left + (right-left)/2
+		if arr[mid] == target {
+			return mid
+		} else if arr[mid] < target {
+			left = mid + 1
+		} else {
+			right = mid - 1
+		}
+	}
+
+	return -1
+}
--- sort/main.go
+++ sort/main.go
@@ -3,12 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	var ar = []int{4, 4, 6, 7, 8, 9, 2, 3, 1, 19}
+	var ar = []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1999}
+	//
+	//// 快排
+	////QuickSort(arr, 9)
+	//countingSort(ar)
+	//fmt.Println(ar)
 
-	// 快排
-	//QuickSort(arr, 9)
-	countingSort(ar)
-	fmt.Println(ar)
+	fmt.Println(bsearch(ar, 99))
 }
 
 // 来个冒泡玩一下
