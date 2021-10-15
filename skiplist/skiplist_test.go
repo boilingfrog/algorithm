35efee1d311d2097b03cd780f0c387f9a6241a30
liz <rick.lizhan@gmail.com>
2021-10-15 15:24:14 +0800

    跳表

--- skiplist/skiplist_test.go
+++ skiplist/skiplist_test.go
@@ -7,4 +7,13 @@ func TestSkipList(t *testing.T) {
 
 	sl.Insert("a", 1)
 	sl.String()
+	t.Log(sl)
+	t.Log(sl.head.forwards[0])
+
+	sl.Insert("b", 2)
+	sl.String()
+	t.Log(sl)
+	t.Log(sl.head.forwards[0])
+	t.Log(sl.head.forwards[0].forwards[0])
+
 }
