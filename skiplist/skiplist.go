98325d6273f015dcf7b594f8ace746ec9af7233b
liz <rick.lizhan@gmail.com>
2021-10-15 09:58:20 +0800

    跳表

--- skiplist/skiplist.go
+++ skiplist/skiplist.go
@@ -1,146 +1,177 @@
 package skiplist
 
 import (
+	"fmt"
 	"math"
 	"math/rand"
-	"sync"
-	"time"
 )
 
-const defaultLevel = 12 // default level
+const (
+	// 最高层数
+	MAX_LEVEL = 16
+)
 
+// 跳表节点结构体
 type skipListNode struct {
-	key  int
-	data interface{}
-	next []*skipListNode
+	// 跳表保存的值
+	v interface{}
+	// 用于排序的分值
+	score int
+	// 层高
+	level int
+	// 每层前进指针
+	forwards []*skipListNode
 }
 
-type skipList struct {
-	head, tail *skipListNode
-	length     int
-	level      int
-	m          sync.RWMutex
-	rand       *rand.Rand
+// 新建跳表节点
+func newSkipListNode(v interface{}, score, level int) *skipListNode {
+	return &skipListNode{v: v, score: score, forwards: make([]*skipListNode, level, level), level: level}
 }
 
-func NewSkipList(opt ...int) *skipList {
-	var level = defaultLevel
-	if opt != nil {
-		level = opt[0]
-	}
-	var sl = &skipList{
-		head:  &skipListNode{next: make([]*skipListNode, level, level)},
-		tail:  &skipListNode{key: math.MinInt32},
-		level: level,
-		rand:  rand.New(rand.NewSource(time.Now().UnixNano())),
-	}
-
-	for l := range sl.head.next {
-		sl.head.next[l] = sl.tail
-	}
-
-	return sl
+// 跳表结构体
+type SkipList struct {
+	// 跳表头结点
+	head *skipListNode
+	// 跳表当前层数
+	level int
+	// 跳表长度
+	length int
 }
 
-func (sl *skipList) Add(key int, data interface{}) {
-	sl.m.Lock()
-	defer sl.m.Unlock()
-
-	// 1. 随机一层用来开始添加该节点
-	l := sl.randomLevel()
-
-	// 2. 从该层还是寻找第一个比key大的节点
-	addPos := make([]*skipListNode, l)
-	node := sl.head
-	for index := l - 1; index >= 0; index-- {
-		for {
-			temp := node.next[index]
-			if temp == sl.tail || temp.key > key {
-				addPos[index] = node // 找到插入节点
-				break
-			} else if temp.key == key {
-				temp.data = data // 如果已经存在节点，就直接更新节点数据
-				return
-			} else {
-				node = temp
-			}
-		}
-	}
-
-	nowNode := &skipListNode{key: key, data: data, next: make([]*skipListNode, l)}
-	for level, pos := range addPos {
-		nowNode.next[level] = pos.next[level]
-		pos.next[level] = nowNode
-	}
-
-	sl.length++
+// 实例化跳表对象
+func NewSkipList() *SkipList {
+	// 头结点，便于操作
+	head := newSkipListNode(0, math.MinInt32, MAX_LEVEL)
+	return &SkipList{head, 1, 0}
 }
 
-func (sl *skipList) randomLevel() int {
-	level := 1
-	for ; level < sl.level && sl.rand.Uint32()&0x1 == 1; level++ {
-	}
-	return level
-}
-
-func (sl *skipList) Remove(key int) bool {
-	sl.m.Lock()
-	defer sl.m.Unlock()
-
-	node := sl.head
-	remPos := make([]*skipListNode, sl.level)
-	var target *skipListNode
-	for level := sl.level - 1; level >= 0; level-- {
-		for {
-			temp := node.next[level]
-			if temp == sl.tail || temp.key > key {
-				break
-			} else if temp.key == key {
-				remPos[level] = node
-				target = temp
-				break
-			} else {
-				node = temp
-			}
-		}
-	}
-
-	if target != nil {
-		for l, pos := range remPos {
-			if pos != nil {
-				pos.next[l] = target.next[l]
-			}
-		}
-		target = nil
-		sl.level--
-	}
-
-	return true
-}
-
-func (sl *skipList) Search(key int) interface{} {
-	sl.m.RLock()
-	defer sl.m.RUnlock()
-
-	node := sl.head
-	for level := sl.level - 1; level >= 0; level-- {
-		for {
-			temp := node.next[level]
-			if temp == sl.tail || temp.key > key {
-				break
-			} else if temp.key == key {
-				return temp.data
-			} else {
-				node = temp
-			}
-		}
-	}
-
-	return false
-}
-
-func (sl *skipList) Len() int {
-	sl.m.Lock()
-	defer sl.m.Unlock()
+// 获取跳表长度
+func (sl *SkipList) Length() int {
 	return sl.length
+}
+
+// 获取跳表层级
+func (sl *SkipList) Level() int {
+	return sl.level
+}
+
+// 插入节点到跳表中
+func (sl *SkipList) Insert(v interface{}, score int) int {
+	if nil == v {
+		return 1
+	}
+
+	// 查找插入位置
+	cur := sl.head
+	// 记录每层的路径
+	update := [MAX_LEVEL]*skipListNode{}
+	i := MAX_LEVEL - 1
+	for ; i >= 0; i-- {
+		for nil != cur.forwards[i] {
+			if cur.forwards[i].v == v {
+				return 2
+			}
+			if cur.forwards[i].score > score {
+				update[i] = cur
+				break
+			}
+			cur = cur.forwards[i]
+		}
+		if nil == cur.forwards[i] {
+			update[i] = cur
+		}
+	}
+
+	// 通过随机算法获取该节点层数
+	level := 1
+	for i := 1; i < MAX_LEVEL; i++ {
+		if rand.Int31()%7 == 1 {
+			level++
+		}
+	}
+
+	// 创建一个新的跳表节点
+	newNode := newSkipListNode(v, score, level)
+
+	// 原有节点连接
+	for i := 0; i <= level-1; i++ {
+		next := update[i].forwards[i]
+		update[i].forwards[i] = newNode
+		newNode.forwards[i] = next
+	}
+
+	// 如果当前节点的层数大于之前跳表的层数
+	// 更新当前跳表层数
+	if level > sl.level {
+		sl.level = level
+	}
+
+	// 更新跳表长度
+	sl.length++
+
+	return 0
+}
+
+// 查找
+func (sl *SkipList) Find(v interface{}, score int) *skipListNode {
+	if nil == v || sl.length == 0 {
+		return nil
+	}
+
+	cur := sl.head
+	for i := sl.level - 1; i >= 0; i-- {
+		for nil != cur.forwards[i] {
+			if cur.forwards[i].score == score && cur.forwards[i].v == v {
+				return cur.forwards[i]
+			} else if cur.forwards[i].score > score {
+				break
+			}
+			cur = cur.forwards[i]
+		}
+	}
+
+	return nil
+}
+
+// 删除节点
+func (sl *SkipList) Delete(v interface{}, score int) int {
+	if nil == v {
+		return 1
+	}
+
+	// 查找前驱节点
+	cur := sl.head
+	// 记录前驱路径
+	update := [MAX_LEVEL]*skipListNode{}
+	for i := sl.level - 1; i >= 0; i-- {
+		update[i] = sl.head
+		for nil != cur.forwards[i] {
+			if cur.forwards[i].score == score && cur.forwards[i].v == v {
+				update[i] = cur
+				break
+			}
+			cur = cur.forwards[i]
+		}
+	}
+
+	cur = update[0].forwards[0]
+	for i := cur.level - 1; i >= 0; i-- {
+		if update[i] == sl.head && cur.forwards[i] == nil {
+			sl.level = i
+		}
+
+		if nil == update[i].forwards[i] {
+			update[i].forwards[i] = nil
+		} else {
+			update[i].forwards[i] = update[i].forwards[i].forwards[i]
+		}
+	}
+
+	sl.length--
+
+	return 0
+}
+
+func (sl *SkipList) String() string {
+	return fmt.Sprintf("level:%+v, length:%+v", sl.level, sl.length)
 }
--- skiplist/skiplist_test.go
+++ skiplist/skiplist_test.go
@@ -2,13 +2,9 @@ package skiplist
 
 import "testing"
 
-func TestSkiplist(t *testing.T) {
-	sl := NewSkipList(1)
-	sl.Add(12, "twelve")
-	sl.Add(11, "eleven")
-	t.Logf("now skip list: %d", sl.Len())
-	t.Logf("get %d from skip : %+v", 11, sl.Search(11))
-	sl.Remove(11)
-	t.Logf("now skip list: %d", sl.Len())
-	t.Logf("get %d from skip : %+v", 11, sl.Search(11))
+func TestSkipList(t *testing.T) {
+	sl := NewSkipList()
+
+	sl.Insert("a", 1)
+	sl.String()
 }
