77a730a66e4a2bfcc58fc7dd213d50afd568663c
liz <rick.lizhan@gmail.com>
2021-10-20 09:44:00 +0800

    搜索二叉树中的插入，删除，最大和最小值的查询

--- tree/binary_search_tree.go
+++ tree/binary_search_tree.go
@@ -8,46 +8,38 @@ type Node struct {
 	right *Node
 }
 
-// BST 是一个节点的值为int类型的二叉搜索树.
-type BST struct {
-	root *Node
-}
-
-// 从根节点依次比较
 func insertNode(root, newNode *Node) {
-	if newNode.value < root.value { // 应该放到根节点的左边
+	if newNode.value < root.value {
 		if root.left == nil {
 			root.left = newNode
 		} else {
 			insertNode(root.left, newNode)
 		}
-	} else if newNode.value > root.value { // 应该放到根节点的右边
+	} else if newNode.value > root.value {
 		if root.right == nil {
 			root.right = newNode
 		} else {
 			insertNode(root.right, newNode)
 		}
 	}
-	// 否则等于根节点
 }
 
 // Insert 插入一个元素.
-func (bst *BST) Insert(value int) {
+func Insert(root *Node, value int) {
 	newNode := &Node{value, nil, nil}
-	// 如果二叉树为空，那么这个节点就当作跟节点
-	if bst.root == nil {
-		bst.root = newNode
+	if root == nil {
+		root = newNode
 	} else {
-		insertNode(bst.root, newNode)
+		insertNode(root, newNode)
 	}
 }
 
 // 查找一个元素
-func (bst *BST) Find(value int) bool {
-	if bst.root == nil {
+func Find(root *Node, value int) bool {
+	if root == nil {
 		return false
 	}
-	search := bst.root
+	search := root
 	for search != nil {
 		if search.value == value {
 			return true
@@ -63,17 +55,107 @@ func (bst *BST) Find(value int) bool {
 	return false
 }
 
+// Delete 删除一个元素.
+func Delete(root *Node, value int) bool {
+	_, existed := remove(root, value)
+	return existed
+}
+
+// 用来递归移除节点的辅助方法.
+// 返回替换root的新节点，以及元素是否存在
+func remove(root *Node, value int) (*Node, bool) {
+	if root == nil {
+		return nil, false
+	}
+	var existed bool
+	// 从左边找
+	if value < root.value {
+		root.left, existed = remove(root.left, value)
+		return root, existed
+	}
+	// 从右边找
+	if value > root.value {
+		root.right, existed = remove(root.right, value)
+		return root, existed
+	}
+	// 如果此节点正是要移除的节点,那么返回此节点，同时返回之前可能需要调整.
+	existed = true
+	// 如果此节点没有孩子，直接返回即可
+	if root.left == nil && root.right == nil {
+		root = nil
+		return root, existed
+	}
+	// 如果左子节点为空, 提升右子节点
+	if root.left == nil {
+		root = root.right
+		return root, existed
+	}
+	// 如果右子节点为空, 提升左子节点
+	if root.right == nil {
+		root = root.left
+		return root, existed
+	}
+	// 如果左右节点都存在,那么从右边节点找到一个最小的节点提升，这个节点肯定比左子树所有节点都大.
+	// 也可以从左子树节点中找一个最大的提升，道理一样.
+	smallestInRight, _ := Min(root.right)
+	// 提升
+	root.value = smallestInRight
+	// 从右边子树中移除此节点
+	root.right, _ = remove(root.right, smallestInRight)
+	return root, existed
+}
+
+// 最小的值
+func Min(root *Node) (int, bool) {
+	if root == nil {
+		return 0, false
+	}
+	min := root.value
+	for root != nil {
+		min = root.value
+		root = root.left
+	}
+
+	return min, true
+}
+
+// 最大的值
+func Max(root *Node) (int, bool) {
+	if root == nil {
+		return 0, false
+	}
+	max := root.value
+	for root != nil {
+		max = root.value
+		root = root.right
+	}
+
+	return max, true
+}
+
 func main() {
-	bst := BST{}
-	bst.Insert(13)
-	bst.Insert(6)
-	bst.Insert(8)
-	bst.Insert(2)
-	bst.Insert(3)
-	bst.Insert(88)
-	bst.Insert(12)
-	bst.Insert(14)
+	// 头结点
+	bst := &Node{
+		value: 13,
+		left:  nil,
+		right: nil,
+	}
+	Insert(bst, 6)
+	Insert(bst, 8)
+	Insert(bst, 2)
+	Insert(bst, 1)
+	Insert(bst, 88)
+	Insert(bst, 12)
+	Insert(bst, 14)
+	fmt.Println(bst.value)
 	fmt.Println("-----------")
-	fmt.Println(bst.Find(12))
+	fmt.Print("查找值是是否存在：")
+	fmt.Println(Find(bst, 12))
+	fmt.Println("-----------")
+	fmt.Print("最小值：")
+	fmt.Println(Min(bst))
+	fmt.Println("-----------")
+	fmt.Print("最大值：")
+	fmt.Println(Max(bst))
 	fmt.Println("-----------")
 }
